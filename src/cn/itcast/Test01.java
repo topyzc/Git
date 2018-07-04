3166ed0c0e0eba88fd838634eeab67d34a05810b
topyzc <40627806+topyzc@users.noreply.github.com>
2018-07-04 13:43:06 +0800

    Update Test01.java

--- src/cn/itcast/Test01.java
+++ src/cn/itcast/Test01.java
@@ -7,8 +7,8 @@ public class Test01 {
 		System.out.println("hello ,github!");
 		System.out.println("hello ,update git!");
 		System.out.println("hello ,update git 02!");
-        System.out.println("hello ,update git 03!");
-        System.out.println("hello ,update git 04!");
+                System.out.println("hello ,update git 03!");
+                System.out.println("hello ,update git 04!");
        
 	}
 
