b101a49337fa16d6ccc0a078b29b647643689c5d
yezhichao <15970321953@163.com>
2018-07-04 13:41:16 +0800

    update file 04

--- src/cn/itcast/Test01.java
+++ src/cn/itcast/Test01.java
@@ -7,7 +7,9 @@ public class Test01 {
 		System.out.println("hello ,github!");
 		System.out.println("hello ,update git!");
 		System.out.println("hello ,update git 02!");
-                System.out.println("hello ,update git 03!");
+        System.out.println("hello ,update git 03!");
+        System.out.println("hello ,update git 04!");
+       
 	}
 
 }
